352755f9b7b614c53333ded36de7ba92af1c1448
alvarocojonudo <alvarocojonudo@gmail.com>
2019-04-15 03:13:53 -0600

    primer commit del curso

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict'
+const mongoose = require("mongoose");
+const app = require('./app');
+const port = process.env.PORT || 3977;
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/curso_mean2",{ useNewUrlParser: true }, (err, res) => {
+    if(err){
+        throw err;
+    }else{
+        //console.log("La base de datos esta correcta!");
+        app.listen(port, function(){
+            //console.log('servidor escuchando.');
+        });
+    }
+});
